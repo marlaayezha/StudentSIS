5f78a60adf687b6abc60609c855467854ad7c897
Sunthunder0813 <lqiiyxahi@gmail.com>
2026-02-21 17:05:22 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace StudentSIS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("STUDENT INFORMATION SYSTEM");
+            Console.WriteLine("\n1. Enroll / Apply");
+            Console.WriteLine("2. Deactivate Account");
+            Console.WriteLine("3. File a Leave");
+            Console.WriteLine("4. Rules");
+            Console.Write("\nEnter: ");
+            string studentChoice = Console.ReadLine();
+
+            if (studentChoice == "1")
+            {
+                Console.Write("\nFirst Name: ");
+                string fName = Console.ReadLine();
+                Console.Write("Last Name: ");
+                string lName = Console.ReadLine();
+                Console.Write("Student Number: ");
+                string studNum = Console.ReadLine();
+                Console.Write("Contact Number: ");
+                string studNum = Console.ReadLine();
+                Console.Write("Address: ");
+                string studNum = Console.ReadLine();
+            }
+        }
+    }
+}
