0e041d01005503dbb1ac4baaeb84e0639b828494
Marla Ayezha Bartolome <>
2026-02-28 16:15:11 +0800

    Updated SIS with List

--- Program.cs
+++ Program.cs
@@ -2,9 +2,14 @@
 using System.Collections.Generic;
 
 namespace StudentSIS
-{
+{ 
     internal class Program
     {
+        static List<string> studFirstName = new List<string>();
+        static List<string> studLastName = new List<string>();
+        static List<string> studNo = new List<string>();
+        static List<string> studContact = new List<string>();
+        static List<string> studAddress = new List<string>();
         static void Main(string[] args)
         {
             Console.WriteLine("STUDENT INFORMATION SYSTEM");
@@ -18,19 +23,23 @@ namespace StudentSIS
 
                 switch (choice){
                     case "1":
-                        studEnroll();
+                        studRegister();
                         break;
 
                 }
             }
         static void menuOpt(){
-                    Console.WriteLine("\n1. Enroll / Apply");
-                    Console.WriteLine("2. Deactivate Account");
-                    Console.WriteLine("3. File a Leave");
-                    Console.WriteLine("4. Rules");
-                    Console.Write("\nEnter: ");
-        }
-        static void studEnroll(){
+                Console.WriteLine("\nMENU");
+                Console.WriteLine("\n1. Register");
+                Console.WriteLine("2. Manage Account");
+                Console.Write("\nEnter: ");
+
+                //Console.WriteLine("\n1. Enroll / Apply");
+                //Console.WriteLine("2. Deactivate Account");
+                //Console.WriteLine("3. File a Leave");
+                //Console.Write("\nEnter: ");
+            }
+            static void studRegister(){
                 Console.Write("\nFirst Name: ");
                     string fName = Console.ReadLine();
                 Console.Write("Last Name: ");
@@ -38,21 +47,29 @@ namespace StudentSIS
                 Console.Write("Student Number: ");
                     string studNum = Console.ReadLine();
                 Console.Write("Contact Number: ");
-                    int contactNum = int.Parse(Console.ReadLine());
+                    string conNum = Console.ReadLine();
                 Console.Write("Address: ");
                     string address = Console.ReadLine();
 
-                Console.Write("Confirm Enrollment? (Y/N): ");
-                    string confirmEnroll = Console.ReadLine();
+                studFirstName.Add(fName);
+                studLastName.Add(lName);
+                studNo.Add(studNum);
+                studContact.Add(conNum);
+                studAddress.Add(address);
 
-                if (confirmEnroll == "y")
-                {
-                    Console.Write("You are now ENROLLED!");
-                }
-                else
-                {
-                    Console.Write("Enrollment Cancelled");
-                }
+
+                //Console.Write("\nConfirm Enrollment? (y/y): ");
+                //    string confirmEnroll = Console.ReadLine();
+
+                //if (confirmEnroll == "y")
+                //{
+                //    Console.Write("\nYou are now ENROLLED!");
+                    
+                //}
+                //else
+                //{
+                //    Console.Write("Enrollment Cancelled");
+                //}
                 
             }
         }
